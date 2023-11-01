3a3cf1df8f5659a8dfd0688b735f15683c09476a
Ramin RX7 <rawmin.rx@gmail.com>
2023-11-01 23:40:19 +0330

    config: add Settings config

---
+++ src/config/settings.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings
+
+
+
+
+class _Config(BaseSettings):
+    MONGODB_URL : str
+
+    REDIS_URL : str
+    REDIS_KEY_TTL : int
+
+    # REDIX : _RedisConfig
+
+    class Config:
+        # env_file = ".env"
+        extra = "ignore"
+        env_nested_delimiter = "_"
+
+
+SETTINGS = _Config()
