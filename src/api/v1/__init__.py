9bf653220b99a55e3959cb86f39f67ef1a1557ec
Ramin RX7 <rawmin.rx@gmail.com>
2023-11-03 14:15:34 +0330

    feat: add podcast list/details routes

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -1,4 +1,20 @@
 from fastapi import APIRouter
 
+from db.podcasts import get_podcast_details, get_podcast_episode_details, get_podcast_episode_list, get_podcast_list
 
 router = APIRouter(prefix='/v1')
+
+
+
+
+@router.get("/podcasts")
+async def podcast_list():
+    res = await get_podcast_list()
+    return res
+
+
+@router.get("/podcast/{id}")
+async def podcast_details(id):
+    res = await get_podcast_details(id)
+    return res
+
